067a9f0026ec282f62655c268b55930203ee8291
olshum <olshum8@gmail.com>
2024-09-15 11:47:41 +0300

    make build

---
+++ index.3474679a.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("li > ul").forEach(function(e){var n=!0,l=e.parentElement,t=l.firstChild,i=document.createElement("span");i.appendChild(t),l.insertBefore(i,e),i.addEventListener("click",function(){n?(e.style.display="none",n=!1):(e.style.display="block",n=!0)})});
+//# sourceMappingURL=index.3474679a.js.map
